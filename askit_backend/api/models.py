df083bd55c5491d21dde82334258fcda7acfd2f9
priyankagaikwad <priya937027@gmail.com>
2025-08-29 17:41:38 +0530

    add

--- askit_backend/api/models.py
+++ askit_backend/api/models.py
@@ -5,11 +5,5 @@ class UserData(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
 
-    def save(self, *args, **kwargs):
-        if not self.pk:  # Only hash password on create
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
-
     class Meta:
-        db_table = 'user_data'  # 👈 This tells Django to use your existing table
-
+        db_table = 'user_data'
