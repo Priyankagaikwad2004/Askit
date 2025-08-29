63ed48b89f05f5f03166052050bf6a74737f8c8d
priyankagaikwad <priya937027@gmail.com>
2025-08-29 11:53:07 +0530

    add

--- askit_backend/places/serializers.py
+++ askit_backend/places/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Place
 
 class PlaceSerializer(serializers.ModelSerializer):
+    fun_facts = serializers.ListField(
+        child=serializers.CharField(), allow_empty=True
+    )
+    visitor_tips = serializers.ListField(
+        child=serializers.CharField(), allow_empty=True
+    )
+
     class Meta:
         model = Place
         fields = [
