81e4b3e78a80d5ef48b9672ed6760058e1a5de53
priyankagaikwad <priya937027@gmail.com>
2025-08-29 16:19:17 +0530

    add

--- askit-frontend/vite.config.js
+++ askit-frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // Make sure this points to your root path
-})
+  base: '/', // important for deployment
+});
