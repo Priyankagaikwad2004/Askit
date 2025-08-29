64b181e84d59ec0082f4bd3eb69c0319a785ef8f
priyankagaikwad <priya937027@gmail.com>
2025-08-29 10:50:53 +0530

    add

--- askit_backend/core/settings.py
+++ askit_backend/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import sys
+import os
+
+# Ensure BASE_DIR is in sys.path
+sys.path.append(os.path.join(BASE_DIR))
 
 # Application definition
 
