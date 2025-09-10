48337ae81d3852d7e384088e74abaf75ad18e981
priyankagaikwad <priya937027@gmail.com>
2025-09-10 19:40:53 +0530

    add

--- askit_backend/api/views.py
+++ askit_backend/api/views.py
@@ -29,15 +29,5 @@ class LoginView(APIView):
     def post(self, request):
         serializer = CustomLoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data['user']  # make sure your serializer returns user
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "access": str(refresh.access_token),
-                "refresh": str(refresh),
-                "user": {
-                    "id": user.id,
-                    "email": user.email,
-                    "name": user.name,
-                }
-            }, status=status.HTTP_200_OK)
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
