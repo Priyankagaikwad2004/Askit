854521b15d0773f089e0518efc4a426d82f10238
priyankagaikwad <priya937027@gmail.com>
2025-09-10 19:37:03 +0530

    serializer

--- askit_backend/api/serializers.py
+++ askit_backend/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import UserData
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 from rest_framework_simplejwt.tokens import RefreshToken
 
+
+# 🔹 For Registration
+class UserDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserData
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Automatically hash the password when creating a new user
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+
+# 🔹 For Login
 class CustomLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
